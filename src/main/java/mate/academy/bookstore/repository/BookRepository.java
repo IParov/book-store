b42860f2004dab1767e4b8a5b41a1b26831454f2
Igor Parovenko <igorparovenko@yahoo.com>
2023-08-31 19:33:10 +0200

    book repository extended from JpaRepository

--- src/main/java/mate/academy/bookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -1,12 +1,8 @@
 package mate.academy.bookstore.repository;
 
-import java.util.List;
-import java.util.Optional;
 import mate.academy.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends GenericRepository<Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
 }
