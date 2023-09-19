d2cd36676f6a8b866bd1727afc10797c95227aa2
Igor Parovenko <igorparovenko@yahoo.com>
2023-09-19 12:35:50 +0200

    getters and setters as class annotation

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -18,45 +18,29 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "books")
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
-@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Long id;
     @Column(nullable = false)
-    @Getter
-    @Setter
     private String title;
     @Column(nullable = false)
-    @Getter
-    @Setter
     private String author;
     @Column(nullable = false, unique = true)
-    @Getter
-    @Setter
     private String isbn;
     @Column(nullable = false)
-    @Getter
-    @Setter
     private BigDecimal price;
-    @Getter
-    @Setter
     private String description;
-    @Getter
-    @Setter
     private String coverImage;
     @Column(nullable = false)
-    @Getter
-    @Setter
     private boolean isDeleted = false;
     @Column(nullable = false)
-    @Getter
-    @Setter
     @ManyToMany
     @JoinTable(name = "books_categories",
             joinColumns = @JoinColumn(name = "book_id"),
--- src/main/java/mate/academy/bookstore/model/Category.java
+++ src/main/java/mate/academy/bookstore/model/Category.java
@@ -13,25 +13,19 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "categories")
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
-@NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Long id;
     @Column(nullable = false, unique = true)
-    @Getter
-    @Setter
     private String name;
-    @Getter
-    @Setter
     private String description;
     @Column(nullable = false)
-    @Getter
-    @Setter
     private boolean isDeleted = false;
 }
